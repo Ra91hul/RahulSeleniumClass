7d554d811331bde4ace94606dc94aede0e6773fc
Ra91hul <88572796+Ra91hul@users.noreply.github.com>
2021-08-10 17:41:32 +0530

    week1 day2 assignment

---
+++ calculator.java
@@ -0,0 +1,29 @@
+package week1.day2.assignments;
+
+public class calculator {
+	// To Check Addition Function
+
+	public void addition(int num1, int num2, int num3) {
+		int sum = (num1 + num2 + num3);
+		System.out.println("Addition result is " + sum);
+	}
+
+	// To Check Subtraction Function
+	public void subtract(int num1, int num2) {
+		int sub = (num1 - num2);
+		System.out.println("Subtraction result is " + sub);
+	}
+
+	// To Check Multiplication Function
+	public void multiply(double num1, double num2) {
+		double mul = (num1 * num2);
+		System.out.println("Multiplication result is " + mul);
+	}
+
+	// To Check Division Function
+	public void divide(float num1, float num2) {
+		float div = (num1 / num2);
+		System.out.println("Division result is " + div);
+	}
+
+}
