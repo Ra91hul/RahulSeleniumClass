b1d012a8faebe0d6a0d2c77407e2aa3fea16388d
Ra91hul <88572796+Ra91hul@users.noreply.github.com>
2021-08-20 23:14:01 +0530

    Add files via upload

---
+++ week2/day2/assignments/RemoveDuplicate.java
@@ -0,0 +1,33 @@
+package week2.day2.assignments;
+
+public class RemoveDuplicate {
+
+	public static void main(String[] args) {
+		// a) Use the declared String text as input
+		String text = "We learn java basics as part of java sessions in java week1";
+
+		// Initialize an integer variable as count
+		int count = 0;
+
+		// c) Split the String into array and iterate over it
+		String[] splitString = text.split(" ");
+
+		for (int i = 0; i < splitString.length; i++) {
+
+			for (int j = i + 1; j < splitString.length; j++) {
+
+				if (splitString[i].equals(splitString[j])) {
+					count = count + 1;
+
+					if (count > 0) {
+						splitString[j]="";
+
+					}
+
+				}
+
+			}
+			System.out.print(splitString[i] + " ");
+		}
+	}
+}
