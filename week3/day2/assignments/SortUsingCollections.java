c373e4fcd1c35191eab0fedb97f61fd76963cfa8
Ra91hul <88572796+Ra91hul@users.noreply.github.com>
2021-08-24 06:46:19 +0530

    Add files via upload

---
+++ week3/day2/assignments/SortUsingCollections.java
@@ -0,0 +1,25 @@
+package week3.day2.assignments;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class SortUsingCollections {
+
+	public static void main(String[] args) {
+		String[] str = { "Aspire Systems", "CTS", "Wipro", "HCL" };
+
+		List<String> list = new ArrayList<String>(Arrays.asList(str));
+		Collections.sort(list);
+
+		for (int i = str.length - 1; i >= 0; i--) {
+			System.out.print(list.get(i));
+			System.out.print(" ");
+
+		}
+
+	}
+
+}
