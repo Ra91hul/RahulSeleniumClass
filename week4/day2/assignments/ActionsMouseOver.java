2b0e40eb639907f9964fc390be87223d07cd4bcd
Ra91hul <88572796+Ra91hul@users.noreply.github.com>
2021-09-01 07:52:11 +0530

    Add files via upload

---
+++ week4/day2/assignments/ActionsMouseOver.java
@@ -0,0 +1,28 @@
+package week4.day2.assignments;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ActionsMouseOver {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/mouseOver.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		WebElement target = driver.findElement(By.linkText("TestLeaf Courses"));
+
+		Actions builder = new Actions(driver);
+		builder.moveToElement(target).perform();
+
+	}
+
+}
---
+++ week4/day2/assignments/JquerySortable.java
@@ -0,0 +1,36 @@
+package week4.day2.assignments;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JquerySortable {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://jqueryui.com/sortable/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.switchTo().frame(0);
+
+		WebElement source = driver.findElement(By.xpath("//li[text()='Item 1']"));
+		WebElement target = driver.findElement(By.xpath("//li[text()='Item 5']"));
+
+		Point location = target.getLocation();
+		int x = location.getX();
+		int y = location.getY();
+
+		Actions builder = new Actions(driver);
+		builder.dragAndDropBy(source, x, y).perform();
+
+	}
+
+}
