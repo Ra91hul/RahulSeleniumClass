120868eb77c82b2c5f6eba5c3c3b064b7a3f7074
Ra91hul <88572796+Ra91hul@users.noreply.github.com>
2021-09-10 06:28:29 +0530

    Add files via upload

---
+++ week5/day2/leaftaps/CommonClass.java
@@ -0,0 +1,37 @@
+package week5.day2.assignments.leaftaps;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CommonClass {
+	
+	public ChromeDriver driver;
+	
+	@Parameters({"username","password","url"})
+	@BeforeMethod
+	public void preCondition(String uName,String pWord, String url) {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys(uName);
+		driver.findElement(By.id("password")).sendKeys(pWord);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+
+	}
+	
+	@AfterMethod
+	public void postCondition() {
+		driver.close();		
+
+	}
+}
